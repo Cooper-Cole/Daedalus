74f455e352445b4e15726e7df68f426d8da2d6f0
Alyssa Cooper <alyssarcoops@gmail.com>
2019-07-31 21:25:20 -0400

    Rough, rough draft of basic functions

--- solar_exchange.sol
+++ solar_exchange.sol
@@ -5,12 +5,18 @@ contract daedalusEnergy {
     uint public energyAmount;
     uint public minBidAmount;
     uint public minBidIncrAmount;
-    address supplier;
-    mapping(address=>bool) bidder;
+    address payable bidder;
     bool completed;
-    address highestBidder;
     uint bidsCount;
     uint minStartBid;
+    uint currentBid;
+    uint surplusId;
+    uint finalBidAmount;
+    bool bidApproved;
+    address highestBidder;
+    address supplier;
+    mapping(address=>uint) public bids;
+    mapping(address=>bool) public accounts;
 
     constructor(uint _bidTime, uint _energyAmount, uint _minBidAmount, uint _minBidIncrAmount) public {
         bidTime = _bidTime;
@@ -19,13 +25,43 @@ contract daedalusEnergy {
         minBidIncrAmount = _minBidIncrAmount;
     }
 
-    function bid(uint _maxBidAmount) public payable {
-        /* Requires bidder address verified, supplier address verified, minBid met */
-        require(bidder[msg.sender], 'Bidder does not exist!');
-        require(bidder[supplier], 'Supplier does not exist!');
-        require(bid[msg.sender] >= msg.value, 'Bid must be at minimum');
-
-        bidder.bid(msg.value);
-
+    struct surplus {
+        bool bidApproved;
+        address highestBidder;
+        address supplier;
+        uint energyAmount;
+        uint highestBid;
+        uint bidsCount;
+        uint bidsTime;
+        uint minBidAmount;
+        uint minBidIncrAmount;
     }
+
+    function createSurplus() public {
+        surplus newSurplus = surplus(
+            {bidApproved: false,
+            highestBidder:2,
+            supplier: address,
+            energyAmount : 2,
+            highestBid : 10,
+            bidsCount: 3,
+            bidsTime: 13,
+            minBidAmount : 0,
+            minBidIncrAmount : 3
+            });
+    }
+
+    function bid(uint _maxBidAmount) public{
+        require(!accounts[msg.sender], 'Bidder exists.');
+        require(!accounts[supplier], 'Recipient exists.');
+        require(bids[msg.sender] > currentBid && bids[msg.sender] > minStartBid,
+        'Bid is greater than current bid and greater than the minimum start bid');
+    }
+
+
+    function finalizeBid(uint _surplusId) public payable {
+        bidder.transfer(msg.value);
+        accounts[supplier] += msg.value;
+    }
+
 }
